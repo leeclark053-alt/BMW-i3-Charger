c72b0f1271584db77108aad3f166d91016cdb21c
leeclark053-alt <leeclark053@googlemail.com>
2026-04-30 11:59:13 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = "bmw-ev-v4";
+
+self.addEventListener("install", e=>{
+
+ e.waitUntil(
+
+  caches.open(CACHE).then(c=>c.addAll([
+
+   "/",
+
+   "/index.html",
+
+   "/manifest.json"
+
+  ]))
+
+ );
+
+});
+
+self.addEventListener("fetch", e=>{
+
+ e.respondWith(
+
+  caches.match(e.request).then(r=>r || fetch(e.request))
+
+ );
+
+});
